44391b89072c1201e80e826baa2434beac70f771
lcwd-team <durgeshkumar8896@gmail.com>
2022-12-20 18:31:38 +0530

    config client

---
+++ ConfigServer/ConfigServer/src/test/java/com/lcwd/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
