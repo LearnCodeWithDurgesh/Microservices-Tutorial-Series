54f778700d2cc08faf4096f45fa0415b10a31164
lcwd-team <durgeshkumar8896@gmail.com>
2022-12-27 19:07:23 +0530

    Implementing circuit breaker using resilience4J

--- UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.lcwd.user.service.controllers;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -24,11 +30,27 @@ public class UserController {
     }
 
     //single user get
+
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        logger.info("Get Single User Handler: UserController");
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
+
+    //creating fall back  method for circuitbreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        logger.info("Fallback is executed because service is down : ", ex.getMessage());
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
 
     //all user get
     @GetMapping
